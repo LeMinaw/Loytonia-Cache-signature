4ec1914f5b873dcf695c2b49d3369aac671eb5ac
Kocal <kocal@live.fr>
2014-03-09 18:40:37 +0100

    On rajoute des pseudos
    On rajoute des pseudos pour lesquels on ne cache pas la signature

--- script.user.js
+++ script.user.js
@@ -1,17 +1,17 @@
 // ==UserScript==
 // @name       Loytonia - On cache les signatures
 // @namespace  http://github.com/Kocal
-// @version    0.1
+// @version    0.2
 // @description  On cache les signatures du forum Loytonia, sauf celle de "Kocal"
 // @match      http://forum.loytonia.com/*
 // @match      https://forum.loytonia.com/*
 // @copyright  2014+, Hugo Alliaume
 // ==/UserScript==
 
-var excludedPseudos = ['kocal'],
+var excludedPseudos = ['kocal', 'avooal', 'josh3582', 'm0tdp', 'michel_liu', 'mybenblack', 'haribooh', 'reexylx', 'c.23-lhc', 'iron_bow', 'rajeshu', 'worms92', 'fireoyster', 'fiskirata33'],
     posts = document.querySelectorAll('tr.post');
 
-for(var i = 0, len = posts.length; i < len; i++) {
+for(var i = 0, len = posts.length; i < len; i++) {w
     var post   = posts[i],
         pseudo = post.querySelector('.name a[href^="/u"] span strong').innerText,
         signature = post.querySelector('.signature_div');
